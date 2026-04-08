65954ee45715cedea65316539a939d657e6e6e99
MKAirborne <mnkpnsr@gmail.com>
2026-04-08 20:55:38 +0530

    Some files modifications

---
+++ Practice/variables & types/Q8.py
@@ -0,0 +1,23 @@
+a = [1,2]
+b = [1,2]
+print(a == b, a is b)
+
+
+
+x = (1)
+print(type(x))
+
+
+
+y = (1,)
+print(type(y))
+
+
+
+c = 3
+d = 3.0
+print(c == d, c is d)
+
+
+
+print(bool([]), bool({}), bool(()))
